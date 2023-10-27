3deba2b4ad238ff527a6472f9e3e7ab976f2900d
Rémi Vaillant <contact.remi.vaillant@gmail.com>
2023-10-27 15:04:59 +0200

    maj tests_unitaires

--- tests_unitaires.py
+++ tests_unitaires.py
@@ -7,6 +7,7 @@ from modelisations import addition, cout_metier
 
 def test_addition():
     '''Assertion de la fonction addition.
+    
     '''
     assert addition() == 0
     assert addition(1, 2) == 3
@@ -15,10 +16,11 @@ def test_addition():
 
 def test_cout_metier():
     '''Assertion de la fonction cout_metier.
+    
     '''
     y_t = np.asarray([0, 0, 0, 1, 1])
     y_p1 = np.asarray([0, 0, 0, 1, 1])
-    y_p2 = np.asarray([0, 0, 0, 1, 1])
+    y_p2 = np.asarray([1, 1, 1, 0, 0])
 
     assert cout_metier(y_t, y_p1) == 0
     assert cout_metier(y_t, y_p2) == 4.6
